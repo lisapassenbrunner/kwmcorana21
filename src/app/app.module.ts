bce2da5825f380a5bfa4855085fa02ae8dc67996
lisapassenbrunner <s1810456024@students.fh-hagenberg.at>
2021-04-30 11:33:35 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,17 @@ import { VaccinationService } from './shared/vaccination.service';
 import { HomeComponent } from './home/home.component';
 import { AppRoutingModule } from "./app-routing.module";
 import { HttpClientModule } from "@angular/common/http";
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeDEAT from '@angular/common/locales/de-AT';
+
+registerLocaleData(localeDEAT);
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
   declarations: [AppComponent, VaccinationListComponent, VaccinationListItemComponent, VaccinationDetailsComponent, HomeComponent],
   bootstrap: [AppComponent],
-  providers: [VaccinationService]
+  providers: [VaccinationService,
+   {provide: LOCALE_ID, useValue: 'de-at'}]
 })
 export class AppModule {}
