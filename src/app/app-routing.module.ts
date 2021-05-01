0f97437d088a6d374990a56e3414b23457159c17
lisapassenbrunner <s1810456024@students.fh-hagenberg.at>
2021-05-01 09:15:13 +0200

    4 files modified, 2 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { Routes, RouterModule } from "@angular/router";
 import { VaccinationDetailsComponent } from "./vaccination-details/vaccination-details.component";
 import { VaccinationListComponent } from "./vaccination-list/vaccination-list.component";
 import { HomeComponent } from "./home/home.component";
+import { VaccinationFormComponent } from "./vaccination-form/vaccination-form.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
   { path: "home", component: HomeComponent },
   { path: "vaccinations", component: VaccinationListComponent },
-  { path: "vaccinations/:code", component: VaccinationDetailsComponent }
+  { path: "vaccinations/:code", component: VaccinationDetailsComponent },
+  { path: "admin", component: VaccinationFormComponent},
+  { path: "admin/:code", component: VaccinationFormComponent}
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { VaccinationListComponent } from "./vaccination-list/vaccination-list.component";
@@ -13,12 +13,13 @@ import { HttpClientModule } from "@angular/common/http";
 import { LOCALE_ID } from '@angular/core';
 import { registerLocaleData } from '@angular/common';
 import localeDEAT from '@angular/common/locales/de-AT';
+import { VaccinationFormComponent } from './vaccination-form/vaccination-form.component';
 
 registerLocaleData(localeDEAT);
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule],
-  declarations: [AppComponent, VaccinationListComponent, VaccinationListItemComponent, VaccinationDetailsComponent, HomeComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
+  declarations: [AppComponent, VaccinationListComponent, VaccinationListItemComponent, VaccinationDetailsComponent, HomeComponent, VaccinationFormComponent],
   bootstrap: [AppComponent],
   providers: [VaccinationService,
    {provide: LOCALE_ID, useValue: 'de-at'}]
