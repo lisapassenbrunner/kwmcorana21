9b0df71549b166e9e11ce4975def4fac6f6f63db
lisapassenbrunner <s1810456024@students.fh-hagenberg.at>
2021-05-02 18:42:33 +0200

    Fehlermeldungen für Ort geht noch nicht

--- src/app/vaccination-form/vaccination-form-error-messages.ts
+++ src/app/vaccination-form/vaccination-form-error-messages.ts
@@ -18,19 +18,45 @@ export const VaccinationFormErrorMessages = [
     "Es muss ein Code für die Impfung angegeben werden"
   ),
   new ErrorMessage(
+    "max_registrations",
+    "required",
+    "Es muss eine maximale Registrierungsanzahl für die Impfung angegeben werden"
+  ),
+  new ErrorMessage(
     "code",
     "minlength",
     "Der Code muss mindestens 5 Zeichen enthalten"
   ),
-  new ErrorMessage(
-    "Date",
-    "required",
-    "Es muss ein Datum angegeben werden"
-  ),
+  new ErrorMessage("Date", "required", "Es muss ein Datum angegeben werden"),
   new ErrorMessage(
     "code",
     "maxlength",
     "Der Code darf höchstens 5 Zeichen haben"
   ),
-  new ErrorMessage('max_registrations', 'max', 'Maximal 15 Personen erlaubt')
+  new ErrorMessage("max_registrations", "max", "Maximal 15 Personen erlaubt"),
+  new ErrorMessage(
+    "title",
+    "required",
+    "Es muss eine Titel für den Impfort angegeben werden"
+  ),
+  new ErrorMessage(
+    "max_registrations",
+    "required",
+    "Es muss eine maximale Registrierungsanzahl für die Impfung angegeben werden"
+  ),
+  new ErrorMessage(
+    "max_registrations",
+    "required",
+    "Es muss eine maximale Registrierungsanzahl für die Impfung angegeben werden"
+  ),
+  new ErrorMessage(
+    "max_registrations",
+    "required",
+    "Es muss eine maximale Registrierungsanzahl für die Impfung angegeben werden"
+  ),
+  new ErrorMessage(
+    "max_registrations",
+    "required",
+    "Es muss eine maximale Registrierungsanzahl für die Impfung angegeben werden"
+  ),
 ];
--- src/app/vaccination-form/vaccination-form.component.ts
+++ src/app/vaccination-form/vaccination-form.component.ts
@@ -1,20 +1,23 @@
 import { ActivatedRoute, Router } from "@angular/router";
 import { Component, OnInit } from "@angular/core";
-import { FormBuilder, FormGroup, FormArray, Validators, FormControl
+import {
+  FormBuilder,
+  FormGroup,
+  FormArray,
+  Validators,
+  FormControl
 } from "@angular/forms";
-import { VaccinationFactory } from '../shared/vaccination-factory';
-import { VaccinationService } from '../shared/vaccination.service';
-import { Vaccination } from '../shared/vaccination';
-import { VaccinationFormErrorMessages } from './vaccination-form-error-messages';
-
+import { VaccinationFactory } from "../shared/vaccination-factory";
+import { VaccinationService } from "../shared/vaccination.service";
+import { Vaccination } from "../shared/vaccination";
+import { VaccinationFormErrorMessages } from "./vaccination-form-error-messages";
 
 @Component({
-  selector: 'kwm-vaccination-form',
-  templateUrl: './vaccination-form.component.html',
-  styleUrls: ['./vaccination-form.component.css']
+  selector: "kwm-vaccination-form",
+  templateUrl: "./vaccination-form.component.html",
+  styleUrls: ["./vaccination-form.component.css"]
 })
 export class VaccinationFormComponent implements OnInit {
-
   vaccinationForm: FormGroup;
   vaccination = VaccinationFactory.empty();
   errors: { [key: string]: string } = {};
@@ -44,83 +47,94 @@ export class VaccinationFormComponent implements OnInit {
   initVaccination() {
     this.buildPlacesArray();
     this.vaccinationForm = this.fb.group({
-    id: this.vaccination.id,
-    description: [this.vaccination.description, Validators.required],
-    vaccine: this.vaccination.vaccine,
-    code: [
-      this.vaccination.code,[
-        Validators.required,
-          Validators.minLength(5),
-          Validators.maxLength(5)
-      ]
-    ],
-    time: this.vaccination.time,
-    max_registrations: [this.vaccination.max_registrations, [Validators.min(5), Validators.max(15)]],
-    places: this.places,
-    date: this.vaccination.date
-  });
+      id: this.vaccination.id,
+      description: [this.vaccination.description, Validators.required],
+      vaccine: this.vaccination.vaccine,
+      code: [
+        this.vaccination.code,
+        [Validators.required, Validators.minLength(5), Validators.maxLength(5)]
+      ],
+      time: this.vaccination.time,
+      max_registrations: [
+        this.vaccination.max_registrations, 
+        [Validators.required, Validators.min(5), Validators.max(15)]
+      ],
+      places: this.places,
+      date: this.vaccination.date
+    });
 
-  this.vaccinationForm.statusChanges.subscribe(() =>
-    this.updateErrorMessages());
+    this.vaccinationForm.statusChanges.subscribe(() =>
+      this.updateErrorMessages()
+    );
   }
 
   buildPlacesArray() {
     this.places = this.fb.array([]);
     for (let place of this.vaccination.place) {
       let fg = this.fb.group({
-        id: new FormControl(place.id), 
+        id: new FormControl(place.id),
         title: new FormControl(place.title, [Validators.required]),
         plz: new FormControl(place.plz, [Validators.required]),
         place: new FormControl(place.place, [Validators.required]),
         street: new FormControl(place.street, [Validators.required]),
         number: new FormControl(place.number, [Validators.required]),
         district: new FormControl(place.district, [Validators.required]),
-        state: new FormControl(place.state, [Validators.required])
       });
       this.places.push(fg);
     }
   }
 
   addPlaceControl() {
-    this.places.push(this.fb.group({ title: null, plz: null
-    , place: null, street: null, number: null, district: null, state: null }));
+    this.places.push(
+      this.fb.group({
+        title: null,
+        plz: null,
+        place: null,
+        street: null,
+        number: null,
+        district: null,
+      })
+    );
   }
 
   submitForm() {
     // filter empty values
     /*this.vaccinationForm.value.places = this.vaccinationForm.value.places.filter(
       thumbnail => thumbnail.url*/
-  //);
+    //);
 
-    const vaccination: Vaccination = VaccinationFactory.fromObject(this.vaccinationForm.value);
+    const vaccination: Vaccination = VaccinationFactory.fromObject(
+      this.vaccinationForm.value
+    );
     //deep copy - did not work without??
-   vaccination.place = this.vaccinationForm.value.places;
+    vaccination.place = this.vaccinationForm.value.places;
     console.log(vaccination);
-  //just copy the authors
+    //just copy the authors
     //vaccination.people = this.vaccination.people;
     if (this.isUpdatingVaccination) {
       this.kwm.update(vaccination).subscribe(res => {
-          this.router.navigate(["../../vaccinations", vaccination.code], {
-        relativeTo: this.route
+        this.router.navigate(["../../vaccinations", vaccination.code], {
+          relativeTo: this.route
+        });
       });
-    });
-  } else {
-        //vaccination.user_id = 1; // jsut for testing
-  // console.log(vaccination);
-        this.kwm.create(vaccination).subscribe(res => {
-          this.vaccination = VaccinationFactory.empty();
-          this.vaccinationForm.reset(VaccinationFactory.empty());
-          this.router.navigate(["../vaccinations"], { relativeTo: this.route});
+    } else {
+      //vaccination.user_id = 1; // jsut for testing
+      // console.log(vaccination);
+      this.kwm.create(vaccination).subscribe(res => {
+        this.vaccination = VaccinationFactory.empty();
+        this.vaccinationForm.reset(VaccinationFactory.empty());
+        this.router.navigate(["../vaccinations"], { relativeTo: this.route });
       });
     }
   }
   updateErrorMessages() {
     console.log("Is invalid? " + this.vaccinationForm.invalid);
+
     this.errors = {};
     for (const message of VaccinationFormErrorMessages) {
+      
       const control = this.vaccinationForm.get(message.forControl);
       if (
-        
         control &&
         control.dirty &&
         control.invalid &&
