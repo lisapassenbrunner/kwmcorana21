b7f7c7dfa8461fb8b75211b0d0e41903a1fc07ea
lisapassenbrunner <s1810456024@students.fh-hagenberg.at>
2021-05-12 16:57:03 +0200

    alles von Authentifizierung mal drinnen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,7 @@ import { VaccinationDetailsComponent } from "./vaccination-details/vaccination-d
 import { VaccinationService } from "./shared/vaccination.service";
 import { HomeComponent } from "./home/home.component";
 import { AppRoutingModule } from "./app-routing.module";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { VaccinationFormComponent } from "./vaccination-form/vaccination-form.component";
 import { LOCALE_ID } from "@angular/core";
 import { registerLocaleData } from "@angular/common";
@@ -23,6 +23,8 @@ import { PersonListItemComponent } from './person-list-item/person-list-item.com
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 import { LoginComponent } from './login/login.component';
 import { AuthenticationService } from './shared/authentication.service';
+import { TokenInterceptorService } from './shared/token-interceptor.service';
+import { JwtInterceptorService } from './shared/jwt-interceptor.service';
 
 registerLocaleData(localeDEAT);
 
@@ -50,6 +52,6 @@ registerLocaleData(localeDEAT);
     LoginComponent
   ],
   bootstrap: [AppComponent],
-  providers: [VaccinationService, PeopleService, AuthenticationService, { provide: LOCALE_ID, useValue: "de-at" }]
+  providers: [VaccinationService, PeopleService, AuthenticationService, TokenInterceptorService, { provide: LOCALE_ID, useValue: "de-at" }, {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptorService, multi: true}]
 })
 export class AppModule {}
---
+++ src/app/shared/jwt-interceptor.service.ts
@@ -0,0 +1,36 @@
+import {
+  HttpErrorResponse,
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+@Injectable()
+export class JwtInterceptorService implements HttpInterceptor {
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+      tap(
+        (event: HttpEvent<any>) => {
+          if (event instanceof HttpResponse) {
+            // do stuff with response if you want
+          }
+        },
+        (err: any) => {
+          if (err instanceof HttpErrorResponse) {
+            if (err.status === 401) {
+              console.log('error');
+              alert('Invalid login');
+            }
+          }
+        }
+      )
+    );
+  }
+}
